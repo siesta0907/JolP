d2042726b6cad3ab0bc70be71d0ea91fbb706969
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2023-12-07 19:35:45 +0900

    머지 작업

---
+++ Assets/Scripts/online_play.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class online_play : MonoBehaviour
+{
+    private Button button;
+    
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        
+        // 버튼에 클릭 이벤트 핸들러를 추가합니다.
+        
+    }
+
+    
+
+    
+}
